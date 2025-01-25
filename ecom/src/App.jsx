3243f386af6fb3030974478483b2f0fbbffc0ff7
Sumanththumma <tsumanth3@gmail.com>
2025-01-25 16:37:25 +0530

    header

--- ecom/src/App.jsx
+++ ecom/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from "./Components/Header.jsx";
+import Hero from "./Components/Hero.jsx";
 const App = () => {
   return (
     <div>
-        <Header/>
+        <Header />
+        <Hero />
     </div>
   )
 }
---
+++ ecom/src/Components/Hero.jsx
@@ -0,0 +1,8 @@
+const Hero = () => {
+    return (
+        <>
+            <div className="w-full h-screen bg-slate-900"></div>
+        </>
+    );
+}
+export default Hero;

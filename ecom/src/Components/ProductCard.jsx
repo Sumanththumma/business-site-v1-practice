1de5d8166bf8868a81a1cde4311361e503a90607
Sumanththumma <tsumanth3@gmail.com>
2025-02-06 21:37:37 +0530

    basic UI polishes

--- ecom/src/Components/ProductCard.jsx
+++ ecom/src/Components/ProductCard.jsx
@@ -2,19 +2,20 @@ import React from 'react'
 import { RiAddCircleFill } from '@remixicon/react'
 
 
+
 const ProductCard = (props) => {
     const addtoCartHandler = () =>{
-      console.log(props.title)
+      setCartCount((prevCartCount)=> prevCartCount+1)
     }
   return (
-    <div className='w-40 md:w-64 md:h-96 h-72 rounded-md px-1 py-1 shadow-xl relative overflow-hidden pt-1'>
+    <div className='w-40 md:w-64 md:h-96 h-72 rounded-md px-1 py-1 shadow-xl relative overflow-hidden pt-1 font-"Montserrat"'>
         <img className='object-cover h-44 md:h-64 w-full rounded-md' src={props.proimage} alt="AJ OG Bred" />
         <div className='flex flex-col gap-2 px-1'>
             <h1 className='font-medium text-md'>{props.title}</h1>
             <p className='text-[12px] font-extralight'>{props.desc}</p>
             <div className='absolute bottom-2 w-[90%] flex justify-between items-center'>
                 <h1>₹ {props.price}.00</h1>
-                <button className='hover:pointer' onClick={addtoCartHandler}><RiAddCircleFill size={32}/></button>
+                <button className=' hover:cursor-pointer' onClick={addtoCartHandler}><RiAddCircleFill size={32}/></button>
             </div>
         </div>
     </div>
--- ecom/src/Components/Shoes.jsx
+++ ecom/src/Components/Shoes.jsx
@@ -8,12 +8,12 @@ const Hero = () => {
                 proimage="https://crepdogcrew.com/cdn/shop/files/1_f1157f4c-7cee-4afb-b73d-9864bf16cf4b.png?v=1737709498&width=1000" 
                 title="Air Jordan 1" 
                 desc="Nike" 
-                price="11,495"/>
+                price="11495"/>
                 <ProductCard 
                 proimage="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/0874f105-ef49-4ce4-91f5-e1494577688d/JORDAN+STADIUM+90.png" 
                 title="Jordan Stadium 90" 
                 desc="Nike" 
-                price="12,795"/>
+                price="12795"/>
                 <ProductCard 
                 proimage="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/b1bcbca4-e853-4df7-b329-5be3c61ee057/NIKE+DUNK+LOW+RETRO.png" 
                 title="Nike Dunk Low Retro" 

b7deb2e3069e36ac1fe6268b43609cfca204fc3a
Sumanththumma <tsumanth3@gmail.com>
2025-02-06 20:56:36 +0530

    Some responsive layout settings

--- ecom/src/Components/ProductCard.jsx
+++ ecom/src/Components/ProductCard.jsx
@@ -7,8 +7,8 @@ const ProductCard = (props) => {
       console.log(props.title)
     }
   return (
-    <div className='w-40 h-72 rounded-md px-1 py-1 shadow-xl relative overflow-hidden pt-1'>
-        <img className='object-cover h-44 w-full rounded-md' src={props.proimage} alt="AJ OG Bred" />
+    <div className='w-40 md:w-64 md:h-96 h-72 rounded-md px-1 py-1 shadow-xl relative overflow-hidden pt-1'>
+        <img className='object-cover h-44 md:h-64 w-full rounded-md' src={props.proimage} alt="AJ OG Bred" />
         <div className='flex flex-col gap-2 px-1'>
             <h1 className='font-medium text-md'>{props.title}</h1>
             <p className='text-[12px] font-extralight'>{props.desc}</p>
--- ecom/src/Components/Shoes.jsx
+++ ecom/src/Components/Shoes.jsx
@@ -3,7 +3,7 @@ import ProductCard from "./ProductCard";
 const Hero = () => {
     return (
         <>
-            <div className="w-full h-screen flex flex-wrap items-center justify-center gap-2 overflow-scroll">
+            <div className="w-full h-screen flex flex-wrap items-center justify-center gap-2 overflow-y-auto">
                 <ProductCard 
                 proimage="https://crepdogcrew.com/cdn/shop/files/1_f1157f4c-7cee-4afb-b73d-9864bf16cf4b.png?v=1737709498&width=1000" 
                 title="Air Jordan 1" 
